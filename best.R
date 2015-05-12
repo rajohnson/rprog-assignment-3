6f6b4d9ea3303932298769c8cfd7226097ed1050
rajohnson <johnson.rob.alan@gmail.com>
2015-05-11 21:29:52 -0700

    started best data aquisition

---
+++ best.R
@@ -0,0 +1,13 @@
+## The 'best' function determines the best hospital in a given state for a given
+## condition(outcome)
+## state can be 2 letter uppercase abbreviation
+## outcome can be “heart attack”, “heart failure”, or “pneumonia"
+## In the event of a tie the first hospital is returned.
+best <- function(state, outcome) {
+  ## Read outcome data
+  
+  ## Check that state and outcome are valid
+  ## Return hospital name in that state with lowest 30-day death
+  ## rate
+  
+}
