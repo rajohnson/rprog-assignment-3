9447256d310485502567c16a0362c7735488743a
rajohnson <johnson.rob.alan@gmail.com>
2015-05-12 10:03:03 -0700

    fixing data manipulation in rank hospital

--- rankhospital.R
+++ rankhospital.R
@@ -19,7 +19,11 @@ rankhospital <- function(state, outcome, num = "best") {
   suppressWarnings(data[,conditioncol] <- as.numeric(data[,conditioncol])) #warnings suppressed, coercing to numeric
   
   ## Return hospital name in that state with the given rank & 30-day death rate
-  data <- subset(data, data[,7] == state)        #trims set for state
-  data <- subset(data, is.na(data[,conditioncol] == TRUE)) #cuts out na for condition
-  
+  data <- subset(data, data[,7] == state)             #trims set for state
+  data <- subset(data, !is.na(data[,conditioncol] == TRUE)) #cuts out na for condition
+  data <- data[,c(2,conditioncol)]                    #cuts off everything but name and conditioncol
+  names(data)[2] <- "Rate"                            #renames conditioncol to "Rate"
+  data <- data[order(data$Rate, data$Hospital.Name),] #sorts by rate, then name
+  data$Rank <- 1:nrow(data)
+  data
 }
