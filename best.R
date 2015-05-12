6e7cc3f1d100b5cf6eda0a86f94e36167b9360c2
rajohnson <johnson.rob.alan@gmail.com>
2015-05-11 21:43:45 -0700

    state error checking

--- best.R
+++ best.R
@@ -1,12 +1,19 @@
 ## The 'best' function determines the best hospital in a given state for a given
 ## condition(outcome)
 ## state can be 2 letter uppercase abbreviation
-## outcome can be “heart attack”, “heart failure”, or “pneumonia"
+## outcome can be "heart attack", "heart failure", or "pneumonia"
 ## In the event of a tie the first hospital is returned.
 best <- function(state, outcome) {
   ## Read outcome data
-  
+  conditioncol <- as.numeric(c("heart attack" = 11, "heart failure" = 17, "pneumonia" = 23)[outcome])
+
+  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  data[,conditioncol] <- as.numeric(data[,conditioncol])
   ## Check that state and outcome are valid
+  if(!exists(state,where=state.abb)){
+    stop("invalid state")
+    return(geterrmessage())
+  }
   ## Return hospital name in that state with lowest 30-day death
   ## rate
   
