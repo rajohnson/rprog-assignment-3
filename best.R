1748869eaf0fc2932e70e691a541c27279d386cc
rajohnson <johnson.rob.alan@gmail.com>
2015-05-11 21:58:31 -0700

    rearrange, outcome error

--- best.R
+++ best.R
@@ -4,17 +4,25 @@
 ## outcome can be "heart attack", "heart failure", or "pneumonia"
 ## In the event of a tie the first hospital is returned.
 best <- function(state, outcome) {
-  ## Read outcome data
-  conditioncol <- as.numeric(c("heart attack" = 11, "heart failure" = 17, "pneumonia" = 23)[outcome])
-
-  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-  data[,conditioncol] <- as.numeric(data[,conditioncol])
-  ## Check that state and outcome are valid
+  
+  # Tie conditioncol to appropriate column for given outcome
+  codes <- c("heart attack" = 11, "heart failure" = 17, "pneumonia" = 23)
+  conditioncol <- as.numeric(codes[outcome])
+  
+  # Check that state and outcome are valid
   if(!exists(state,where=state.abb)){
     stop("invalid state")
     return(geterrmessage())
   }
-  ## Return hospital name in that state with lowest 30-day death
-  ## rate
+  if(is.na(conditioncol)){
+    stop("invalid outcome")
+    return(geterrmessage())    
+  }
+  
+  # Read outcome data
+  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  data[,conditioncol] <- as.numeric(data[,conditioncol])
+ 
+  # Return hospital name in that state with lowest 30-day death rate
   
 }
